ed2dd5818c92e19a206c404473500d8cd2625ade
edson <edsoncarlos.dev@gmail.com>
2022-09-15 12:10:58 -0300

    feat: add GameBanners with data from back-end

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import { CreateAdBanner } from './components/CreateAdBanner';
 import { GameBanner } from './components/GameBanner';
 
@@ -5,9 +7,25 @@ import './styles/main.css';
 
 import logoImg from './assets/logo-nlw-esports.svg';
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
 
 function App() {
+  const [games, setGames] = useState<Game[]>([])
 
+  useEffect(() => {
+    fetch('http://localhost:3000/games')
+    .then(response => response.json())
+    .then(data => {
+      setGames(data);
+    })
+  }, [])
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
@@ -17,8 +35,16 @@ function App() {
       </h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={1}/>
-        <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={0}/>
+        {games.map(game => {
+          return (
+            <GameBanner 
+              key={game.id}
+              bannerUrl={game.bannerUrl} 
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
       </div>
 
       <CreateAdBanner />
