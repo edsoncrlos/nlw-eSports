d794957794bed735e3cf957d9429d8936f38fa57
edson <edsoncarlos.dev@gmail.com>
2022-09-15 21:10:57 -0300

    feat: add modal with lib radix-ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
-import { useState, useEffect } from 'react';
-
+import * as Dialog from '@radix-ui/react-dialog';
+import { useEffect, useState } from 'react';
 import { CreateAdBanner } from './components/CreateAdBanner';
 import { GameBanner } from './components/GameBanner';
 
 import './styles/main.css';
 
 import logoImg from './assets/logo-nlw-esports.svg';
+
 
 interface Game {
   id: string;
@@ -47,7 +48,18 @@ function App() {
         })}
       </div>
 
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <Dialog.Portal>
+          <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
+
+          <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
+            <Dialog.Title className='text-3xl font-black'>Publique um anúncio</Dialog.Title>
+              
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   )
 }
