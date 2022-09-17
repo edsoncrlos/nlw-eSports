693a17eed1c936b65f03e770d6cd24d6dd977af2
edson <edsoncarlos.dev@gmail.com>
2022-09-17 09:03:37 -0300

    feat: add games with server data and IP environment variable

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,14 +1,16 @@
-import { Text, TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { ImageBackground, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 
-import { styles } from './styles';
 import { THEME } from '../../theme';
+import { styles } from './styles';
 
 export interface GameCardProps {
     id: string;
-    name: string;
-    ads: string;
-    cover: ImageSourcePropType;
+    title: string;
+    _count: {
+        ads: number
+    };
+    bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps{
@@ -20,7 +22,7 @@ export function GameCard({data, ...rest}: Props) {
     <TouchableOpacity style={styles.container} {...rest}>
         <ImageBackground
             style={styles.cover}
-            source={data.cover}
+            source={{ uri: data.bannerUrl }}
         >
 
 
@@ -29,11 +31,11 @@ export function GameCard({data, ...rest}: Props) {
                 style={styles.footer}    
                 >
                 <Text style={styles.name}>
-                    {data.name}
+                    {data.title}
                 </Text>
 
                 <Text style={styles.ads}>
-                    {data.ads} anúncios
+                    {data._count.ads} anúncios
                 </Text>
             </LinearGradient>
         </ImageBackground>
