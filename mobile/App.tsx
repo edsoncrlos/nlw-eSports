4620f80b9e1a7dfff56bf18a876ee837efdca013
edson <edsoncarlos.dev@gmail.com>
2022-09-18 09:26:42 -0300

    feat: add base to use notification in mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 
 import {
@@ -11,6 +12,13 @@ import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
 
+import { Subscription } from 'expo-modules-core';
+
+import * as Notifications from 'expo-notifications';
+import './src/services/notificationConfigs'
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
+
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -18,6 +26,30 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   });
+  
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+  
+  useEffect(() => {
+    getPushNotificationToken();
+  })
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification);
+    });
+    
+    responseNotificationListener.current = Notifications.addNotificationReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  }, []);
 
   return (
     <Background>
